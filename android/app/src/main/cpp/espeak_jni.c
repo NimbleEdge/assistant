40b5832f00f8aae15077206c401a309288f9ddfd
Puneet Jindal <puneet.jindal@nimbleedgehq.ai>
2025-08-11 15:56:55 +0530

    Cosmetics
    Signed-off-by: Puneet Jindal <puneet.jindal@nimbleedgehq.ai>

--- android/app/src/main/cpp/espeak_jni.c
+++ android/app/src/main/cpp/espeak_jni.c
@@ -1,13 +1,14 @@
 /*
-* SPDX-FileCopyrightText: (C) 2025 DeliteAI Authors
-*
-* SPDX-License-Identifier: Apache-2.0
-*/
+ * SPDX-FileCopyrightText: (C) 2025 DeliteAI Authors
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 #include <android/log.h>
 #include <jni.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "espeak_ng.h"
 #include "speak_lib.h"
 
@@ -15,7 +16,7 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
-static int voice_initialized;
+static int voice_initialized = 0;
 
 JNIEXPORT jint JNICALL
 Java_dev_deliteai_assistant_domain_features_tts_espeak_EspeakManager_nativeInitialize(
